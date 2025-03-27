036b152257b1efe0819f8b1bc45d16a4eb165b58
MartinSDampier <martinsdampier@gmail.com>
2025-03-27 13:54:51 -0500

    Add basic sliders

--- lib/Views/NegotiationView.ts
+++ lib/Views/NegotiationView.ts
@@ -19,6 +19,43 @@ export class NegotiationView extends ItemView {
     }
     async onOpen() {
         this.contentEl.empty();
+
+        let patienceLabel = this.contentEl.createEl('label', {text: 'Patience'})
+        this.contentEl.createEl('br');
+        let patience = this.contentEl.createEl('input', {type:'range'})
+        let patienceDataList = this.contentEl.createEl('datalist');
+        let intrigueLabel = this.contentEl.createEl('label', {text: 'Intrique'})
+        this.contentEl.createEl('br');
+        let intrigue = this.contentEl.createEl('input', {type:'range'})
+        let intrigueDataList = this.contentEl.createEl('datalist');
+
+        patienceDataList.id = 'values';
+        patienceDataList.style.width = '80%'
+        for (let i = 0; i < 6; i++)
+        {
+            let option = patienceDataList.createEl('option', {value: i.toString()});
+            option.label = i.toString();
+        }
+        
+        patience.setAttribute('list', 'values')
+        patience.min = "0";
+        patience.max = "5";
+        patience.step = "1";
+        patience.style.width = '80%'
+
+        intrigueDataList.id = 'values';
+        intrigueDataList.style.width = '80%'
+        for (let i = 0; i < 6; i++)
+        {
+            let option = intrigueDataList.createEl('option', {value: i.toString()});
+            option.label = i.toString();
+        }
+
+        intrigue.setAttribute('list', 'values')
+        intrigue.min = "0";
+        intrigue.max = "5";
+        intrigue.step = "1";
+        intrigue.style.width = '80%'
     }
     
     async onClose() {
